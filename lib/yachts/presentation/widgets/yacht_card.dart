1add8659d6e6f096228e2ae63712271ea6687d60
Rob <rob.bebbington@outlook.com>
2020-04-21 19:23:08 +1000

    Finished the list screen

--- lib/yachts/presentation/widgets/yacht_card.dart
+++ lib/yachts/presentation/widgets/yacht_card.dart
@@ -11,17 +11,19 @@ class YachtCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Card(
-        
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            Text(yacht.name,
-            style: TextStyle(fontSize: 30,),),
-            SizedBox(height: 10,),
-            Text('IMO: ${yacht.imo.toString()}',),
-            SizedBox(height: 10,),
-            Text('Length: ${yacht.length.toString()}',),
-          ],
+        margin: EdgeInsets.all(5),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text(yacht.name,
+              style: TextStyle(fontSize: 30,),),
+              SizedBox(height: 10,),
+              Text('IMO: ${yacht.imo.toString()}',),
+              SizedBox(height: 10,),
+              Text('Length: ${yacht.length.toString()}',),
+            ],
+          ),
         ),
       ),
     );
